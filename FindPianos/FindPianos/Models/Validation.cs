958dcea82fddd742d7ce49d56b855998d382cef9
maximz <none@none>
2010-06-22 23:56:50 -0700

    Implemented all validation for models.
    Revised todo list.

--- FindPianos/FindPianos/Models/Validation.cs
+++ FindPianos/FindPianos/Models/Validation.cs
@@ -6,6 +6,95 @@ using System.Data.Linq;
 
 namespace FindPianos.Models
 {
+    #region Validation Models
+    public partial class PianoUserSuspension
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            if (string.IsNullOrEmpty(Reason))
+                yield return new RuleViolation("A suspension reason is required.", "Reason");
+            if (ReinstateDate == null)
+                yield return new RuleViolation("A reinstate date is required.", "ReinstateDate");
+            else if (ReinstateDate <= SuspensionDate)
+                yield return new RuleViolation("Reinstate date should be after suspension date.", "ReinstateDate");
+
+            yield break;
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving");
+
+        }
+    }
+    public partial class PianoListingFlag
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            using(var db = new PianoDataContext())
+            {
+                if (ListingID == null)
+                    yield return new RuleViolation("Invalid listing.", "ListingID");
+                else if (db.PianoListings.Where(l => l.PianoID == this.ListingID).Count() != 1)
+                    yield return new RuleViolation("Invalid listing.", "ListingID");
+                if (TypeID == null)
+                    yield return new RuleViolation("Invalid flag type.", "TypeID");
+                else if (db.PianoFlagTypes.Where(t => t.FlagTypeID == this.TypeID).Count() != 1)
+                    yield return new RuleViolation("Invalid flag type.", "TypeID");
+
+            yield break;
+            }
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving");
+
+        }
+    }
+    public partial class PianoReviewFlag
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            using (var db = new PianoDataContext())
+            {
+                if (ReviewID == null)
+                    yield return new RuleViolation("Invalid review.", "ReviewID");
+                else if (db.PianoReviews.Where(l => l.PianoReviewID == this.ReviewID).Count() != 1)
+                    yield return new RuleViolation("Invalid review.", "ReviewID");
+                if (TypeID == null)
+                    yield return new RuleViolation("Invalid flag type.", "TypeID");
+                else if (db.PianoFlagTypes.Where(t => t.FlagTypeID == this.TypeID).Count() != 1)
+                    yield return new RuleViolation("Invalid flag type.", "TypeID");
+
+                yield break;
+            }
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving");
+
+        }
+    }
     public partial class PianoReviewComment
     {
         public bool IsValid
@@ -36,7 +125,7 @@ namespace FindPianos.Models
 
         }
     }
-    public partial class PianoReviewRevision
+    public partial class PianoListingComment
     {
         public bool IsValid
         {
@@ -45,9 +134,67 @@ namespace FindPianos.Models
 
         public IEnumerable<RuleViolation> GetRuleViolations()
         {
-            if (RatingOverall == null)
-                yield return new RuleViolation("Overall rating is required", "RatingOverall");
+            if (!string.IsNullOrEmpty(this.MessageText))
+            {
+                if (this.MessageText.Trim() == "")
+                {
+                    yield return new RuleViolation("No text entered for this comment.", "Message");
+                }
+            }
+            else
+                yield return new RuleViolation("No text entered for this comment.", "Message");
 
+            yield break;
+
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving.");
+
+        }
+    }
+    public partial class PianoReview
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            using (var db = new PianoDataContext())
+            {
+                if (this.PianoListingID == null)
+                    yield return new RuleViolation("Listing ID is required", "ListingID");
+                else if (db.PianoListings.Where(l=>l.PianoID==this.PianoListingID).Count() != -1)
+                    yield return new RuleViolation("Listing doesn't exist", "ListingID");
+
+                yield break;
+            }
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving");
+
+        }
+    }
+    public partial class PianoListing
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            if (Lat == null || Long == null)
+                yield return new RuleViolation("We weren't able to locate that street address.", "StreetAddress");
+            if (string.IsNullOrEmpty(StreetAddress))
+                yield return new RuleViolation("A street address is required", "StreetAddress");
             yield break;
         }
 
@@ -58,7 +205,77 @@ namespace FindPianos.Models
 
         }
     }
+    public partial class PianoReviewRevision
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
 
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            using(var db = new PianoDataContext())
+            {
+                if (PianoReviewID == null)
+                    yield return new RuleViolation("PianoReview is required", "PianoReviewID");
+                else if (db.PianoReviews.Where(r => r.PianoReviewID == this.PianoReviewID).Count() != 1)
+                    yield return new RuleViolation("PianoReview is invalid", "PianoReviewID");
+                if (PianoStyleID == null)
+                    yield return new RuleViolation("PianoStyle is required", "PianoStyleID");
+                else if (db.PianoStyles.Where(s => s.PianoStyleID== this.PianoStyleID).Count() != 1)
+                    yield return new RuleViolation("PianoStyle is invalid", "PianoStyleID");
+                if (RatingOverall == null)
+                    yield return new RuleViolation("Overall rating is required", "RatingOverall");
+                if (PricePerHourInUSD == null)
+                    yield return new RuleViolation("Price per hour is required", "PricePerHourInUSD");
+                else if (PricePerHourInUSD < 0)
+                    yield return new RuleViolation("Price per hour must not be less than 0", "PricePerHourInUSD");
+
+                yield break;
+            }
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving");
+
+        }
+    }
+    public partial class PianoVenueHour
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            using(var db = new PianoDataContext())
+            {
+                if (ReviewRevisionID == null)
+                    yield return new RuleViolation("Review revision ID is required.", "ReviewRevisionID");
+                else if (db.PianoReviewRevisions.Where(r => r.PianoReviewRevisionID == this.ReviewRevisionID).Count() != 1)
+                    yield return new RuleViolation("Review revision ID is invalid.", "ReviewRevisionID");
+                if (DayOfWeek == null)
+                    yield return new RuleViolation("Day of week is required.", "DayOfWeek");
+                else if (db.WeekDays.Where(d => d.WeekDayID == this.DayOfWeek).Count() != 1)
+                    yield return new RuleViolation("Day of week is invalid.", "DayOfWeek");
+
+                yield break;
+            }
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving");
+
+        }
+    }
+    #endregion
+
+    #region Base Validation Class(es)
 
     /// <summary>
     /// Denotes a violation of a validation rule of a certain Type
@@ -84,4 +301,6 @@ namespace FindPianos.Models
             PropertyName = propertyName;
         }
     }
+    
+    #endregion
 }
