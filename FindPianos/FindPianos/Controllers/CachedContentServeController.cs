af45f5ccfb1bdf94fff0f4b482baf968540d0102
MaximZ <none@none>
2010-06-29 16:18:02 -0700

    Fixed problems with cached content not being served up.

--- FindPianos/FindPianos/Controllers/CachedContentServeController.cs
+++ FindPianos/FindPianos/Controllers/CachedContentServeController.cs
@@ -8,22 +8,40 @@ using RiaLibrary.Web;
 
 namespace FindPianos.Controllers
 {
+    /// <summary>
+    /// Caches content.
+    /// </summary>
     [HandleError]
     [OutputCache(Duration = 7200, VaryByParam = "None")] //two hours
     public class CachedContentServeController : Controller
     {
-        [Url("static/{id}/png")]
+        /// <summary>
+        /// Returns PNG images in the Content directory.
+        /// </summary>
+        /// <param name="id">The file name (without .png).</param>
+        /// <returns></returns>
+        [Url("Content/{id}/png")]
         public ActionResult PngImage(string id)
         {
             return File(MakeContentPath("Content",id,"png"), "image/png");
         }
-        [Url("static/{id}/css")]
+        /// <summary>
+        /// Returns CSS files in the Content directory.
+        /// </summary>
+        /// <param name="id">The file name (without .css).</param>
+        /// <returns></returns>
+        [Url("Content/{id}/css")]
         public ActionResult StyleSheet(string id)
         {
             return File(MakeContentPath("Content",id, "css"), "text/css");
         }
+        /// <summary>
+        /// Returns a version of jQuery.min.js.
+        /// </summary>
+        /// <param name="jqueryVersion">The requested version of jQuery.</param>
+        /// <returns></returns>
         [OutputCache(Duration=4838400, VaryByParam="jqueryVersion")] //8 weeks
-        [Url("scripts/jQuery/{jqueryVersion}")]
+        [Url("Scripts/jQuery/{jqueryVersion}")]
         public ActionResult jQuery(string jqueryVersion)
         {
             return File(MakeContentPath("Scripts","jquery-"+jqueryVersion+".min", "js"), "text/javascript");
@@ -39,7 +57,7 @@ namespace FindPianos.Controllers
         /// <returns>A string with the combined path.</returns>
         private string MakeContentPath(string dirName, string fileName, string fileExtension)
         {
-            var dir = Server.MapPath(dirName);//Server.MapPath("Content");
+            var dir = Server.MapPath("../../" + dirName);//Server.MapPath("Content");
             var path = Path.Combine(dir, fileName + "."+fileExtension.Replace(".",""));
             return path;
         }
