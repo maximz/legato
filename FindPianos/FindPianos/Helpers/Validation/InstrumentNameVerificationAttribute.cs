50ab7d8a8db75e101a0f0ae11c3f6beaec2fbbb6
MaximZ <none@none>
2010-07-08 16:25:36 -0700

    Completed and documented InstrumentNameVerificationAttribute.

--- FindPianos/FindPianos/Helpers/Validation/InstrumentNameVerificationAttribute.cs
+++ FindPianos/FindPianos/Helpers/Validation/InstrumentNameVerificationAttribute.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using FindPianos.Models;
 
 namespace FindPianos.Helpers
 {
+    /// <summary>
+    /// Verifies that the given instrument name exists as an instrument within our database.
+    /// </summary>
     public class InstrumentNameVerificationAttribute : ValidationAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InstrumentNameVerificationAttribute"/> class.
+        /// </summary>
         public InstrumentNameVerificationAttribute() : base()
         {
             ErrorMessage = "That instrument doesn't exist in our system.";
         }
+        /// <summary>
+        /// Determines whether the specified value is valid.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// 	<c>true</c> if the specified value is valid; otherwise, <c>false</c>.
+        /// </returns>
         public override bool IsValid(object value)
         {
             if (value == null) //we don't care if it's required or not.
@@ -19,19 +33,18 @@ namespace FindPianos.Helpers
                 return true;
             }
             var address = (string)value;
-            //TODO: check in DB whether an instrument with such a name exists
-            throw new NotImplementedException();
-            //if (price < MinPrice)
-            //{
-            //    return false;
-            //}
-            //double cents = price - Math.Truncate(price);
-            //if (cents < 0.99 || cents >= 0.995)
-            //{
-            //    return false;
-            //}
-
-            return true;
+            try
+            {
+                using (var db = new LegatoDataContext())
+                {
+                    db.Instruments.Where(i => i.Name == value.ToString()).Single();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
