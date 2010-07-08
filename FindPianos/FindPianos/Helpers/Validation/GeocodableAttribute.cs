088f83754d570d4ba6ae4fd8cf664a8e4ada27bc
MaximZ <none@none>
2010-07-08 16:22:31 -0700

    Documentation.

--- FindPianos/FindPianos/Helpers/Validation/GeocodableAttribute.cs
+++ FindPianos/FindPianos/Helpers/Validation/GeocodableAttribute.cs
@@ -6,19 +6,34 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FindPianos.Helpers
 {
+	/// <summary>
+	/// Verifies that a certain property is geocodable; if true, writes the geocoding results to attributes of your choosing. To be applied to an entire class.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 	public class GeocodableAttribute : ValidationAttribute
 	{
+		/// <summary>
+		/// Gets or sets the name of the address property.
+		/// </summary>
+		/// <value>The name of the address property.</value>
 		public string AddressPropertyName
 		{
 			get;
 			set;
 		}
+		/// <summary>
+		/// Gets or sets the name of the latitude property.
+		/// </summary>
+		/// <value>The name of the latitude property.</value>
 		public string LatitudePropertyName
 		{
 			get;
 			set;
 		}
+		/// <summary>
+		/// Gets or sets the name of the longitude property.
+		/// </summary>
+		/// <value>The name of the longitude property.</value>
 		public string LongitudePropertyName
 		{
 			get;
@@ -39,11 +54,21 @@ namespace FindPianos.Helpers
 			var propertyInfo = type.GetProperty(propertyName);
 			propertyInfo.SetValue(obj, value, null);
 		}
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GeocodableAttribute"/> class.
+		/// </summary>
 		public GeocodableAttribute()
 			: base()
 		{
 			ErrorMessage = "We weren't able to find that location.";
 		}
+		/// <summary>
+		/// Determines whether the specified value is valid.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>
+		/// 	<c>true</c> if the specified value is valid; otherwise, <c>false</c>.
+		/// </returns>
 		public override bool IsValid(object value)
 		{
 			var address = GetPropertyValue(value, AddressPropertyName);
