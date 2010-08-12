6d0d6fee346358a28da840a87a0d72d78324ee4c
maximz <none@none>
2010-08-11 23:57:21 -0700

    Added [CustomCache] usage to SiteMetadataController.

--- FindPianos/FindPianos/Controllers/SiteMetadataController.cs
+++ FindPianos/FindPianos/Controllers/SiteMetadataController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RiaLibrary.Web;
+using FindPianos.Helpers;
 
 namespace FindPianos.Controllers
 {
     public class SiteMetadataController : CustomControllerBase
     {
         [Url("sitemap.xml")]
+        [CustomCache(AllowOnlyValidSearchEngines=true,Duration=120)] //2 minutes
         public ActionResult SiteMap()
         {
             throw new NotImplementedException();
         }
         [Url("robots.txt")]
+        [CustomCache(AllowOnlyValidSearchEngines = false, Duration = 432000)] //5 days
         public ActionResult Robots()
         {
             return File(Server.MapPath("robots.txt.config"),"text/plain");
