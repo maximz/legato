45f6c5121f4d7fb4477325e5a4eed95368e1266f
MaximZ <none@none>
2010-08-13 12:34:06 -0700

    added dbproj

---
+++ FindPianos/FindPianos.Data/Schema Objects/FindPianos.Data.rolememberships.sql
@@ -0,0 +1,6 @@
+﻿EXECUTE sp_addrolemember @rolename = N'db_datareader', @membername = N'pianotoiletapp';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_datawriter', @membername = N'pianotoiletapp';
+
