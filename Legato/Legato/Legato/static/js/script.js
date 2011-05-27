976e7b1c8d20e85bc10097cc2f4f2b638a14b21b
maximz <maxim@maximzaslavsky.com>
2011-05-27 13:37:57 -0700

    Added BluePrint CSS

--- Legato/Legato/Legato/static/js/script.js
+++ Legato/Legato/Legato/static/js/script.js
@@ -1,3 +1,36 @@
 $(function(){
-
+    /* Name: Demo
+    Author: Demo King */
+    /*demo namespace*/
+    demo = {
+        common: {
+            init: function () {
+                //initialize
+            },
+            finalize: function () {
+                //finalize
+            },
+            config: {
+                prop: "my value",
+                constant: "42"
+            }
+        },
+        mapping: {
+            init: function () {
+                //create a map
+            },
+            geolocate: function () {
+                //geolocation is cool
+            },
+            geocode: function () {
+                //look up an address or landmark
+            },
+            drawPolylines: function () {
+                //draw some lines on a map
+            },
+            placeMarker: function () {
+                //place markers on the map
+            }
+        }
+    }
 });
