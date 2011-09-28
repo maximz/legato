758bc78b5ff00e5be6a315fea5f5fe74e6901578
maximz <maxim@maximzaslavsky.com>
2011-09-27 19:38:53 -0700

    Changed links on HomeController.Index.cshtml to be correct.
    --HG--
    branch : release1

--- Legato/Legato/Legato/Properties/AssemblyInfo.cs
+++ Legato/Legato/Legato/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.36.1733")]
-[assembly: AssemblyFileVersion("1.0.36.1733")]
+[assembly: AssemblyVersion("1.0.38.1809")]
+[assembly: AssemblyFileVersion("1.0.38.1809")]
