709d62b518a4cb52f53d472933334e14f8b26773
maximz <maxim@maximzaslavsky.com>
2011-11-25 03:03:27 -0800

    Updated legal UI

--- Legato/Legato/Legato/Properties/AssemblyInfo.cs
+++ Legato/Legato/Legato/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.79.0204")]
-[assembly: AssemblyFileVersion("1.0.79.0204")]
+[assembly: AssemblyVersion("1.0.80.0300")]
+[assembly: AssemblyFileVersion("1.0.80.0300")]
