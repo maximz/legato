816eee3d409c0d7573e768a57960fefbad9a17dc
maximz <maxim@maximzaslavsky.com>
2011-11-24 19:06:54 -0800

    Publishing and deploying beta version.
    --HG--
    branch : release1

--- Legato/Legato/Legato/Properties/AssemblyInfo.cs
+++ Legato/Legato/Legato/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.64.1848")]
-[assembly: AssemblyFileVersion("1.0.64.1848")]
+[assembly: AssemblyVersion("1.0.65.1906")]
+[assembly: AssemblyFileVersion("1.0.65.1906")]
