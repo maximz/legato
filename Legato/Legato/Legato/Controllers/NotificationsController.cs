e0bc2ef67779c71a335c559a9ce54e1fe5d78a50
maximz <maxim@maximzaslavsky.com>
2011-12-22 00:40:49 -0800

    Sanity checks in Notifications

--- Legato/Legato/Legato/Controllers/NotificationsController.cs
+++ Legato/Legato/Legato/Controllers/NotificationsController.cs
@@ -9,51 +9,60 @@ using System.Web.Security;
 
 namespace Legato.Controllers
 {
-    [HandleError]
-    public class NotificationsController : CustomControllerBase
-    {
-        [CustomAuthorization(AuthorizeEmailNotConfirmed = true, AuthorizeSuspended = true)]
-        public virtual ActionResult List()
-        {
-            ClearNotifications((Guid)Membership.GetUser().ProviderUserKey);
-            return View(GetNotifications((Guid)Membership.GetUser().ProviderUserKey));
-        }
-
-
-        public List<Notification> GetNotifications(Guid userid)
+	[HandleError]
+	public class NotificationsController : CustomControllerBase
 	{
-		var cachekey = "notifications-"+userid;
-            var currentCache = Current.GetCachedObject(cachekey);
-		if(currentCache != null)
+		[CustomAuthorization(AuthorizeEmailNotConfirmed = true, AuthorizeSuspended = true)]
+		public virtual ActionResult List()
 		{
-            return currentCache as List<Notification>;
+			ClearNotifications((Guid)Membership.GetUser().ProviderUserKey);
+			return View(GetNotifications((Guid)Membership.GetUser().ProviderUserKey));
 		}
-		var db = Current.DB;
-		var notifications = db.Notifications.Where(n=>n.UserID == (Guid)Membership.GetUser().ProviderUserKey && n.IsUnread).OrderByDescending(n=>n.Date).ToList();
-        Current.SetCachedObject(cachekey, notifications, 5 * 60);
-        return notifications;
+
+
+		public List<Notification> GetNotifications(Guid userid)
+		{
+			if(userid == null)
+			{
+				return null;
+			}
+			var cachekey = "notifications-"+userid;
+			var currentCache = Current.GetCachedObject(cachekey);
+			if(currentCache != null)
+			{
+				return currentCache as List<Notification>;
+			}
+			var db = Current.DB;
+			var notifications = db.Notifications.Where(n=>n.UserID == (Guid)Membership.GetUser().ProviderUserKey && n.IsUnread).OrderByDescending(n=>n.Date).ToList();
+			Current.SetCachedObject(cachekey, notifications, 5 * 60);
+			return notifications;
+		}
+
+		public bool GetState()
+		{
+			var results = GetNotifications((Guid)Membership.GetUser().ProviderUserKey);
+			if (results != null && results.Count() >= 1)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public void ClearNotifications(Guid userid)
+		{
+			if(userid == null)
+			{
+				return;
+			}
+			var cachekey = "notifications-" + userid;
+			var db = Current.DB;
+			foreach (var n in db.Notifications.Where(n => n.UserID == (Guid)Membership.GetUser().ProviderUserKey && n.IsUnread))
+			{
+				n.IsUnread = false;
+			}
+			db.SubmitChanges();
+
+			Current.RemoveCachedObject(cachekey);
+		}
 	}
-
-        public bool GetState()
-        {
-            if (GetNotifications((Guid)Membership.GetUser().ProviderUserKey) != null)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public void ClearNotifications(Guid userid)
-        {
-            var cachekey = "notifications-" + userid;
-            var db = Current.DB;
-            foreach (var n in db.Notifications.Where(n => n.UserID == (Guid)Membership.GetUser().ProviderUserKey && n.IsUnread))
-            {
-                n.IsUnread = false;
-            }
-            db.SubmitChanges();
-
-            Current.RemoveCachedObject(cachekey);
-        }
-    }
 }
