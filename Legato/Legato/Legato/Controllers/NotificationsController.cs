cf830f890f0877a25a30ba7e0a120bf01b9a05b5
maximz <maxim@maximzaslavsky.com>
2011-12-23 00:40:39 -0800

    NotificationsController: added CreateNotification(); added documentaton; added route

--- Legato/Legato/Legato/Controllers/NotificationsController.cs
+++ Legato/Legato/Legato/Controllers/NotificationsController.cs
@@ -6,13 +6,19 @@ using System.Web.Mvc;
 using Legato.Helpers;
 using Legato.Models;
 using System.Web.Security;
+using RiaLibrary.Web;
 
 namespace Legato.Controllers
 {
 	[HandleError]
-    public partial class NotificationsController : CustomControllerBase
+	public partial class NotificationsController : CustomControllerBase
 	{
+		/// <summary>
+		/// Lists a user's notification.
+		/// </summary>
+		/// <returns></returns>
 		[CustomAuthorization(AuthorizeEmailNotConfirmed = true, AuthorizeSuspended = true)]
+		[Url("notifications")]
 		public virtual ActionResult List()
 		{
 			ClearNotifications((Guid)Membership.GetUser().ProviderUserKey);
@@ -20,7 +26,12 @@ namespace Legato.Controllers
 		}
 
 
-		public List<Notification> GetNotifications(Guid userid)
+		/// <summary>
+		/// Gets the notifications.
+		/// </summary>
+		/// <param name="userid">The userid.</param>
+		/// <returns></returns>
+		internal List<Notification> GetNotifications(Guid userid)
 		{
 			if(userid == null)
 			{
@@ -34,15 +45,19 @@ namespace Legato.Controllers
 			}
 			var db = Current.DB;
 			var notifications = db.Notifications.Where(n=>n.UserID == (Guid)Membership.GetUser().ProviderUserKey && n.IsUnread).OrderByDescending(n=>n.Date).ToList();
-            foreach(var n in notifications)
-            {
-                n.GlobalPostID1.FillProperties();
-            }
+			foreach(var n in notifications)
+			{
+				n.GlobalPostID1.FillProperties();
+			}
 			Current.SetCachedObject(cachekey, notifications, 5 * 60);
 			return notifications;
 		}
 
-		public bool GetState()
+		/// <summary>
+		/// Gets the state.
+		/// </summary>
+		/// <returns></returns>
+		internal bool GetState()
 		{
 			var results = GetNotifications((Guid)Membership.GetUser().ProviderUserKey);
 			if (results != null && results.Count() >= 1)
@@ -52,7 +67,11 @@ namespace Legato.Controllers
 			return false;
 		}
 
-		public void ClearNotifications(Guid userid)
+		/// <summary>
+		/// Clears the notifications.
+		/// </summary>
+		/// <param name="userid">The userid.</param>
+		internal void ClearNotifications(Guid userid)
 		{
 			if(userid == null)
 			{
@@ -67,6 +86,28 @@ namespace Legato.Controllers
 			db.SubmitChanges();
 
 			Current.RemoveCachedObject(cachekey);
+		}
+
+		/// <summary>
+		/// Creates the notification.
+		/// </summary>
+		/// <param name="userid">The userid.</param>
+		/// <param name="post">The post.</param>
+		/// <param name="time">The time.</param>
+		/// <returns></returns>
+		internal Notification CreateNotification(Guid userid, GlobalPostID post, DateTime time)
+		{
+			var db = Current.DB;
+			var notification = new Notification();
+			notification.UserID = userid;
+			notification.IsUnread = true;
+			notification.GlobalPostID = post.GlobalPostID1;
+			notification.Date = time;
+
+			db.Notifications.InsertOnSubmit(notification);
+			db.SubmitChanges();
+
+			return notification;
 		}
 	}
 }
