0e9f03396cf1398390eca52f0aad48d6cc1edf82
maximz <maxim@maximzaslavsky.com>
2011-12-25 00:17:13 -0800

    Wasn't able to get debug/release web.config transformations to work

--- Legato/Legato/Legato/Properties/AssemblyInfo.cs
+++ Legato/Legato/Legato/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.187.2324")]
-[assembly: AssemblyFileVersion("1.0.187.2324")]
+[assembly: AssemblyVersion("1.0.189.0012")]
+[assembly: AssemblyFileVersion("1.0.189.0012")]
