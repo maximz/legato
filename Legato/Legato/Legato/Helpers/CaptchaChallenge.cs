a373a3e8b709eece6e06a14ae9e2d17bb101b3bf
maximz <maxim@maximzaslavsky.com>
2011-12-25 00:52:50 -0800

    Collecting stuff for a future CaptchaChallenge feature

---
+++ Legato/Legato/Legato/Helpers/CaptchaChallenge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Legato.Helpers
+{
+    public class CaptchaChallenge
+    {
+        /*public void dostuff()
+        {
+            dynamic filterContext;
+            var request = Current.Request;
+            filterContext.Controller.ViewData["RequestedUrl"] = request.Url.ToString();
+            serialize:
+            request.Params
+                request as as whole
+
+            filterContext.Result = new ViewResult { ViewName = "CaptchaChallenge" };
+
+        }*/
+    }
+}
