01f8fca2bfaea1b963deba3c3dbdcac316a6c211
maximz <maxim@maximzaslavsky.com>
2011-12-25 00:53:58 -0800

    Added noearth JS

--- Legato/Legato/Legato/Properties/AssemblyInfo.cs
+++ Legato/Legato/Legato/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.190.0052")]
-[assembly: AssemblyFileVersion("1.0.190.0052")]
+[assembly: AssemblyVersion("1.0.191.0053")]
+[assembly: AssemblyFileVersion("1.0.191.0053")]
