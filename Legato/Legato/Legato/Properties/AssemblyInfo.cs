52061c716d27e968f03a735ddeaf96618ca99da0
maximz <maxim@maximzaslavsky.com>
2011-12-31 01:22:58 -0800

    Tabified home page

--- Legato/Legato/Legato/Properties/AssemblyInfo.cs
+++ Legato/Legato/Legato/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.226.2020")]
-[assembly: AssemblyFileVersion("1.0.226.2020")]
+[assembly: AssemblyVersion("1.0.228.2001")]
+[assembly: AssemblyFileVersion("1.0.228.2001")]
